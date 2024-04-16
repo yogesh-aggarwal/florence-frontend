4303368ee9e9b673222ec1500579bfde1dcf13e2
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-04-16 23:01:57 +0530

    added image to the webpage when cart is empty

--- src/Routes/Cart.tsx
+++ src/Routes/Cart.tsx
@@ -5,8 +5,8 @@ import useRazorpay, { RazorpayOptions } from "react-razorpay";
 import { useCart, useProducts, useUser, cartStore } from "../Lib/State";
 import { Cart as CartActions } from "../Lib/cart";
 import { networkRequest } from "../Lib/helpers";
-import Topbar from "../components/Topbar";
 import { useNavigate } from "react-router-dom";
+import EmpltycartImg from "../assets/emptycart.svg";
 
 type CartProduct_t = {
   id: string;
@@ -33,7 +33,6 @@ function CheckoutButton(props: { finalPrice: number }) {
     if (order.status !== 200) return;
 
     const orderMeta = await order.json();
-    console.log(orderMeta);
 
     const options: RazorpayOptions = {
       key: "rzp_test_rMU9G0FV33EqNz", // Enter the Key ID generated from the Dashboard
@@ -51,7 +50,6 @@ function CheckoutButton(props: { finalPrice: number }) {
           userId: user._id,
         });
         if (orderPlaced.status === 200) {
-          console.log(300);
           const order = await orderPlaced.json();
           cartStore.reset();
           navigate(`/order/${order["orderId"]}`);
@@ -100,9 +98,16 @@ export default function Cart() {
   const [total, setTotal] = useState<number>(0);
   const [delivery, setDelivery] = useState<number>(0);
   const [discount, setDiscount] = useState<number>(0);
+  const [cartValue, setCartValue] = useState<number>(0);
 
   const taxes = Math.ceil(total * 0.18);
   const finalPrice = total + taxes + delivery - discount;
+
+  useEffect(() => {
+    let num = { ...cartStore.value() };
+    let arr = Object.keys(num);
+    setCartValue(arr.length);
+  }, [cart]);
 
   useEffect(() => {
     const productIDS = Object.keys(cart);
@@ -111,19 +116,19 @@ export default function Cart() {
     let sum = 0;
     const coveredProducts = new Set<string>();
     for (const product of products) {
-      if (coveredProducts.has(product._id)) continue;
-      coveredProducts.add(product._id);
+      if (coveredProducts.has(product.id)) continue;
+      coveredProducts.add(product.id);
 
-      if (productIDS.includes(product._id)) {
+      if (productIDS.includes(product.id)) {
         result.push({
           title: product.title,
-          id: product._id,
+          id: product.id,
           image: product.images[0],
           starRatings: product.starRatings,
           price: product.price,
-          quantity: cart[product._id],
+          quantity: cart[product.id],
         });
-        sum += product.price * cart[product._id];
+        sum += product.price * cart[product.id];
       }
     }
 
@@ -136,104 +141,120 @@ export default function Cart() {
 
   return (
     <div className="CartComponent">
-      <Topbar></Topbar>
-      <div className="body">
-        <div className="left">
-          <div className="heading">Cart</div>
-          <div className="cards">
-            {cartProducts.map((currentProduct, i) => {
-              return (
-                <div key={i} className="card">
-                  <img src={currentProduct.image} alt="" />
-                  <div className="info">
-                    <div className="basic">
-                      <div className="title">{currentProduct.title}</div>
-                      <div className="stars">
-                        <i className="fi fi-sr-star"></i>
-                        <span>{currentProduct.starRatings} stars</span>
-                      </div>
-                    </div>
-                    <div className="price">
-                      <div className="count">
-                        <div
-                          className="button"
-                          onClick={() => {
-                            CartActions.add(currentProduct!.id);
-                          }}
-                        >
-                          +
-                        </div>
-                        <span>{cart[currentProduct.id]}</span>
-                        <div
-                          className="button"
-                          onClick={() => {
-                            CartActions.deleteProduct(currentProduct!.id);
-                          }}
-                        >
-                          -
+      {!cartValue ? (
+        <div className="noEle">
+          <img src={EmpltycartImg} alt="" />
+        </div>
+      ) : (
+        <div className="body">
+          <div className="left">
+            <div className="heading">Cart</div>
+            <div className="cards">
+              {cartProducts.map((currentProduct, i) => {
+                return (
+                  <div key={i} className="card">
+                    <img src={currentProduct.image} alt="" />
+                    <div className="info">
+                      <div className="basic">
+                        <div className="title">{currentProduct.title}</div>
+                        <div className="stars">
+                          <i className="fi fi-sr-star"></i>
+                          <span>{currentProduct.starRatings} stars</span>
                         </div>
                       </div>
-                      <div className="total">
-                        <span>₹</span>
-                        <span>
-                          {+currentProduct.price * cart[currentProduct.id]}
-                        </span>
+                      <div className="price">
+                        <div className="count">
+                          <div
+                            className="button"
+                            onClick={() => {
+                              CartActions.add(currentProduct!.id);
+                            }}
+                          >
+                            +
+                          </div>
+                          <span>{cart[currentProduct.id]}</span>
+                          <div
+                            className="button"
+                            onClick={() => {
+                              CartActions.deleteProduct(currentProduct!.id);
+                            }}
+                          >
+                            -
+                          </div>
+                        </div>
+                        <div className="total">
+                          <span>₹</span>
+                          <span>
+                            {+currentProduct.price * cart[currentProduct.id]}
+                          </span>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })}
+            </div>
           </div>
-        </div>
-        <div className="right">
-          <div className="heading">Reciept</div>
-          <div className="content">
-            <div className="list">
-              <div className="top">
-                <div className="item head">
-                  <span>Product name</span>
-                  <span>Q</span>
-                  <span>Price</span>
-                </div>
-                {cartProducts.map((product) => {
-                  return (
-                    <div className="item">
-                      <span>{product.title}</span>
-                      <span>{product.quantity}</span>
-                      <span>₹{product.price}</span>
-                    </div>
-                  );
-                })}
+          <div className="right">
+            <div className="head">
+              <div className="heading">Reciept</div>
+              <div
+                className="clearCart"
+                onClick={() => {
+                  CartActions.clear();
+                }}
+              >
+                <i className="fi fi-sr-trash"></i>
+                Clear Cart
               </div>
-              <div className="bottom">
-                <div className="item">
-                  <span>Taxes</span>
-                  <span></span>
-                  <span>₹{taxes}</span>
+            </div>
+            <div className="content">
+              <div className="list">
+                <div className="top">
+                  <div className="item head">
+                    <span>Product name</span>
+                    <span>Q</span>
+                    <span>Price</span>
+                  </div>
+                  {cartProducts.map((product) => {
+                    return (
+                      <div className="item">
+                        <span>{product.title}</span>
+                        <span>{product.quantity}</span>
+                        <span>₹{product.price}</span>
+                      </div>
+                    );
+                  })}
                 </div>
-                <div className="item">
-                  <span>Delivery charges</span>
-                  <span></span>
-                  <span>₹{delivery}</span>
+                <div className="bottom">
+                  <div className="item">
+                    <span>Taxes</span>
+                    <span></span>
+                    <span>₹{taxes}</span>
+                  </div>
+                  <div className="item">
+                    <span>Delivery charges</span>
+                    <span></span>
+                    <span>₹{delivery}</span>
+                  </div>
+                  <div className="item discount">
+                    <span>Discount</span>
+                    <span></span>
+                    <span>-₹{discount}</span>
+                  </div>
                 </div>
-                <div className="item discount">
-                  <span>Discount</span>
-                  <span></span>
-                  <span>-₹{discount}</span>
+              </div>
+              <div className="cta">
+                <CheckoutButton finalPrice={finalPrice} />
+                <div className="total">
+                  <span>₹</span>
+                  <span>{finalPrice}</span>
                 </div>
               </div>
             </div>
-            <div className="cta">
-              <CheckoutButton finalPrice={finalPrice} />
-              <div className="total">
-                <span>₹</span>
-                <span>{finalPrice}</span>
-              </div>
-            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
